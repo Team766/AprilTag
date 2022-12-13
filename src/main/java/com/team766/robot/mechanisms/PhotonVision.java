b3d3236c640d9d889acd4279f295f586f05df770
Alexandre Sauquet <54484828+SauquetAlex@users.noreply.github.com>
2022-12-13 19:38:03 +0000

    Some transform 3d stuff

--- src/main/java/com/team766/robot/mechanisms/PhotonVision.java
+++ src/main/java/com/team766/robot/mechanisms/PhotonVision.java
@@ -3,16 +3,37 @@ package com.team766.robot.mechanisms;
 import com.team766.framework.Mechanism;
 import com.team766.hal.MotorController;
 import com. team766.hal.RobotProvider;
+import edu.wpi.first.math.geometry.Transform3d;
 import org.photonvision.*;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import java.util.*;
 
 public class PhotonVision extends Mechanism {	
     PhotonCamera camera;
-    
+    List<PhotonTrackedTarget> targets;
+    PhotonTrackedTarget target;
+
     public PhotonVision(){
         camera = new PhotonCamera("photonvision");
         var result = camera.getLatestResult();
-        List<PhotonTrackedTarget> targets = result.getTargets();
+        if(result.hasTargets()){
+            targets = result.getTargets();
+            target = result.getBestTarget();
+        }
+
     }
+    public void update(){
+        var result = camera.getLatestResult();
+        if(result.hasTargets()){
+            targets = result.getTargets();
+            target = result.getBestTarget();
+        }
+    }
+    public int getID (){
+        return target.getFiducialId();
+    }
+    public Transform3d getTransform3d(){
+        return target.getBestCameraToTarget();
+    }
+
 }
