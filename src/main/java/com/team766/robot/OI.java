dda47d4995dd6f147ee9f4d72548fe921c0cd14b
Alexandre Sauquet <54484828+SauquetAlex@users.noreply.github.com>
2022-12-17 19:31:05 +0000

    12/17

--- src/main/java/com/team766/robot/OI.java
+++ src/main/java/com/team766/robot/OI.java
@@ -31,8 +31,15 @@ public class OI extends Procedure {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
 			context.takeOwnership(Robot.drive);
+			context.takeOwnership(Robot.photonVision);
 			Robot.drive.setArcadeDrivePower(joystick0.getAxis(1), joystick1.getAxis(0));
-			
+			log();
+			//log the x,y,z, and angle of the target
+			log("x", Robot.photonVision.getXYZAngle().get(0));
+			log("y", Robot.photonVision.getXYZAngle().get(1));
+			log("z", Robot.photonVision.getXYZAngle().get(2));
+			log("angle", Robot.photonVision.getXYZAngle().get(3));
+			// Wait until the next update from the driver station.
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}
 	}
